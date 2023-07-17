8d39d843bf692c36a14bcea5e338893ea643f425
KimSun <1565833208@qq.com>
2023-07-17 13:55:52 +0800

    fix: 隐藏搜索方案

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -368,18 +368,18 @@ const config = {
   },
   themes: [
     // ... Your other themes.
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-      ({
-        // `hashed` is recommended as long-term-cache of index file is possible.
-        hashed: true,
-        language: ["en", "zh"],
-        indexBlog: true,
-        indexPages: true,
-        indexDocs: true,
-      }),
-    ],
+    // [
+    //   require.resolve("@easyops-cn/docusaurus-search-local"),
+    //   /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+    //   ({
+    //     // `hashed` is recommended as long-term-cache of index file is possible.
+    //     hashed: true,
+    //     language: ["en", "zh"],
+    //     indexBlog: true,
+    //     indexPages: true,
+    //     indexDocs: true,
+    //   }),
+    // ],
   ],
 }
 
